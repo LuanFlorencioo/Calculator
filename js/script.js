82d2dd4c420a4cca7221df07949f5d1a9f990f17
Gabriel Silva <luanflorencio87@gmail.com>
2022-03-15 19:24:39 -0300

    file update

--- js/script.js
+++ js/script.js
@@ -1,73 +1,93 @@
-const getAllButtons = Array.from(document.querySelectorAll('button'));
-getAllButtons.forEach(button => () => { button.preventDefault() })
+const calc = {
+    equation: '',
+    addValue(value) {
+        this.equation += value
+        return console.log(this.equation)
+    },
+    getLastValue() {
+        const lastValue = this.equation.at(-1);
+        return lastValue;
+    },
+    removeLastValue() {
+        this.equation = this.equation.slice(0, -1)
+        this.modifyScreen();
+        return console.log(this.equation)
+    },
+    cleanEquation() {
+        this.equation = '';
+        this.modifyScreen();
+        return console.log(this.equation)
+    },
+    calculateEquation() {
+        try {
+            const result = eval(this.equation);
+            if (isNaN(result) || !isFinite(result))
+                this.equation = '0'
+            else
+                this.equation = `${result}`
 
-let equacion = ''
-
-const insertValue = (e) => {
-    const lastValue = equacion[equacion.length - 1]
-
-    if (+lastValue || lastValue === '0' || equacion === '' && e.target.classList[2] === '-') {
-        equacion += e.target.classList[2]
-        console.log(equacion)
+            this.modifyScreen();
+            return console.log(this.equation)
+        }
+        catch {
+            alert('Ops! Parece que a sua operação está incorreta. Verifique novamente. Talvez seja um operador ou algum número fora de lugar.')
+        }
+    },
+    modifyScreen() {
+        const screen = document.querySelector('.screen-result');
+        screen.innerText = this.equation;
     }
-    else {
-        if (e.target.classList[1] === 'number') {
-            equacion += e.target.classList[2]
-            console.log(equacion)
-        }
-        else {
-            console.log('Não dá pra inserir dois sinais!')
-        }
-    }
-}
+};
 
-const getValueButtons = Array.from(document.querySelectorAll('button.value'))
-getValueButtons.forEach(button => button.addEventListener('click', insertValue))
+{
 
-const deleteValue = () => {
-    const newValue = equacion.slice(0, equacion.length - 1);
-    equacion = newValue
-    console.log(equacion)
-}
+    const insertValueClick = ({ target }) => {
+        const key = target.classList[2];
+        const lastValue = calc.getLastValue();
 
-const getDeleteButton = document.querySelector('button.delete')
-getDeleteButton.addEventListener('click', deleteValue)
-
-const resetValue = () => {
-    equacion = ''
-    console.log('valor resetado!')
-}
-
-const getResetButton = document.querySelector('button.reset')
-getResetButton.addEventListener('click', resetValue)
-
-const equalValue = () => {
-    const lastValue = equacion[equacion.length - 1];
-
-    if (+lastValue || lastValue === '0') {
-
-        const finalResult = eval(equacion);
-
-        if (isNaN(finalResult) || !isFinite(finalResult)) {
-            console.log('valor quebrado!')
-        }
-        else {
-            const resultSplit = String(finalResult).split('.');
-
-            if (resultSplit[1].length > 4) {
-                equacion = `${resultSplit[0]}.${resultSplit[1].slice(0, 4)}`
+        if (lastValue) {
+            if (isNaN(+lastValue) && isNaN(+key)) {
+                console.error("Erro202: Já foi inserido outro operador matemático");
             }
             else {
-                equacion = `${finalResult}`
-            }
-            console.log(equacion)
+                calc.addValue(key);
+                calc.modifyScreen();
 
+            }
+        }
+        else {
+            if (key == '-' || !isNaN(+key)) {
+                calc.addValue(key)
+                calc.modifyScreen();
+
+            }
+            else {
+                console.error('Erro101: Primeiro você deve inserir um número')
+            }
         }
     }
-    else {
-        console.log('cálculo em falso!')
-    }
-}
 
-const getEqualButton = document.querySelector('button.equal')
-getEqualButton.addEventListener('click', equalValue)
+    const getAllKeys = Array.from(document.querySelectorAll('button'));
+    getAllKeys.forEach(keyButton => {
+        switch (keyButton.classList[0]) {
+            case 'value':
+                keyButton.addEventListener('click', insertValueClick);
+                break;
+
+            case 'delete':
+                keyButton.addEventListener('click', () => calc.removeLastValue());
+                break;
+
+            case 'reset':
+                keyButton.addEventListener('click', () => calc.cleanEquation());
+                break;
+
+            case 'equal':
+                keyButton.addEventListener('click', () => calc.calculateEquation());
+                break;
+
+            default:
+                break;
+        }
+    })
+}
