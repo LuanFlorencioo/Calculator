6d1c42017bee756690d115b90663bc16f2b57e18
Gabriel Silva <luanflorencio87@gmail.com>
2022-03-22 15:53:38 -0300

    file update

--- js/script.js
+++ js/script.js
@@ -1,135 +1,179 @@
 const calc = {
     equation: '',
-    addValue(value) {
-        calc.equation += value;
-        calc.changeResultScreen();
-        document.querySelector('.calc-screen').scroll(300, 0)
-    },
-    changeResultScreen() {
-        const screen = document.querySelector('.screen-result');
-        screen.innerText = calc.equation;
-    },
+
     valueVerification(newValue) {
-        const lastValue = calc.getLastValue();
 
-        if (lastValue) {
-            if (isNaN(+lastValue) && isNaN(+newValue))
-                calc.errorMessage("Ops! Não pode haver dois operadores juntos. Verifique os valores novamente.");
+        const getLastValue = () => {
+            const lastValue = calc.equation.at(-1);
+            return lastValue;
+        };
+        const isNumberTheLastValue = !isNaN(+getLastValue());
+        const isNumberTheNewValue = !isNaN(+newValue);
+        const isValueCorrect = (isNumberTheLastValue && isNumberTheNewValue) || (isNumberTheLastValue && !isNumberTheNewValue) || (!isNumberTheLastValue && isNumberTheNewValue);
 
-            else
-                calc.addValue(newValue);
-        }
-        else {
-            if (newValue == '-' || !isNaN(+newValue))
-                calc.addValue(newValue);
+        const addValue = (value) => {
+            calc.equation += value;
+            calc.changeResultScreen();
+            document.querySelector('.calc-screen').scroll(300, 0);
+        };
 
-            else
-                calc.errorMessage('Para começar, você deve iniciar inserindo um número ou o valor de menos(-).');
-        }
+        if (getLastValue()) {
+            (isValueCorrect) ? addValue(newValue) : calc.errorMessage("Oops! It is not possible to have two operators together. check the values again.");
+        } else {
+            (newValue == '-' || isNumberTheNewValue) ? addValue(newValue) : calc.errorMessage('First, you must start by entering a number or the value of minus (-).');
+        };
     },
-    getLastValue() {
-        const lastValue = calc.equation.at(-1);
-        return lastValue;
-    },
-    removeLastValue() {
-        calc.equation = calc.equation.slice(0, -1);
-        calc.changeResultScreen();
-    },
-    resetEquation() {
-        calc.equation = '';
-        calc.changeResultScreen();
-    },
+
     errorMessage(errorAlert) {
         const modal = document.querySelector('.modalIn');
-        modal.querySelector('p').innerText = errorAlert;
+        const modalNotActivated = !modal.classList.contains('active');
 
-        if (!modal.classList.contains('active')) {
+        const showTheModal = () => {
+            modal.querySelector('p').innerText = errorAlert;
             modal.classList.toggle('active');
+
             setTimeout(() => {
                 modal.classList.replace('modalIn', 'modalOut')
             }, 4000);
+
             setTimeout(() => {
                 modal.classList.toggle('active')
                 modal.classList.replace('modalOut', 'modalIn')
             }, 5000);
         }
+
+        (modalNotActivated) ?
+            showTheModal() :
+            console.warn("Wait a second! There is a modal explaining the error.");
     },
+
+    changeResultScreen() {
+        const screen = document.querySelector('.screen-result');
+        screen.innerText = calc.equation;
+    },
+
+    removeLastValue() {
+        calc.equation = calc.equation.slice(0, -1);
+        calc.changeResultScreen();
+    },
+
+    resetEquation() {
+        calc.equation = '';
+        calc.changeResultScreen();
+    },
+
     calculateEquation() {
         try {
-            const resultEquation = eval(calc.equation);
-            if (calc.equation == '0/0')
-                calc.errorMessage('Zero dividido por zero??? HaHaHa você só pode está brincando comigo.');
+            const finalResultEquation = eval(calc.equation);
+            const isResultNumber = !isNaN(finalResultEquation);
+            const isResultFinite = isFinite(finalResultEquation);
 
-            if (isNaN(resultEquation) || !isFinite(resultEquation))
+            if (calc.equation == '0/0')
+                calc.errorMessage('Zero divided by zero??? HAHAHA you can only be kidding me');
+
+            (isResultNumber || isResultFinite) ?
+                calc.equation = `${finalResultEquation}` :
                 calc.equation = '0';
-            else
-                calc.equation = `${resultEquation}`;
 
             calc.changeResultScreen();
             document.querySelector('.calc-screen').scroll(0, 0)
         }
         catch {
-            calc.errorMessage('Ops! Sua operação está incorreta. Verifique novamente. Talvez seja um operador ou algum número fora de lugar.')
-            console.warn(errorAlert);
+            calc.errorMessage("Oops! Your operation is incorrect. Check again. Maybe it's an operator or some number out of place.")
         }
     }
 }
 
 const body = document.body;
-const currentTheme = window.getComputedStyle(body).backgroundColor;
-if (currentTheme == 'rgb(58, 71, 100)')
-    body.setAttribute('data-theme', '1');
-else
+const toggle = document.querySelector('.toggle');
+const getAllButtons = Array.from(document.querySelectorAll('button'));
+const systemColorCurrent = window.getComputedStyle(body).backgroundColor;
+(systemColorCurrent == 'rgb(58, 71, 100)') ?
+    body.setAttribute('data-theme', '1') :
     body.setAttribute('data-theme', '3');
 
 const changeNextTheme = () => {
     const idCurrentTheme = body.dataset.theme;
-    if (idCurrentTheme == '3')
-        body.dataset.theme = '1';
-    else
+    (idCurrentTheme == '3') ?
+        body.dataset.theme = '1' :
         body.dataset.theme = +(body.dataset.theme) + 1;
 }
-
-const toggle = document.querySelector('.toggle');
 toggle.addEventListener('click', changeNextTheme);
 
+
+const insertValueClick = ({ type, key, target }) => {
+    const handleKeyup = () => {
+        const isNumberKey = () => {
+            const numberKey = !isNaN(+key);
+            if (numberKey)
+                return key;
+        }
+
+        const isOperationKey = () => {
+            const operationKey = (key === '+') ||
+                (key === '-') ||
+                (key === '*') ||
+                (key === '/') ||
+                (key === '.');
+            if (operationKey)
+                return key;
+        };
+
+        switch (key) {
+            case isNumberKey():
+                calc.valueVerification(key);
+                break;
+
+            case isOperationKey():
+                calc.valueVerification(key);
+                break;
+
+            case 'Backspace':
+                calc.removeLastValue();
+                break;
+
+            case 'Enter':
+                calc.calculateEquation();
+                break;
+
+            case 'c':
+                calc.resetEquation();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    const handleClick = () => {
+        const valueClicked = target.classList[2];
+        calc.valueVerification(valueClicked);
+    }
+
+    (type == 'keyup') ?
+        handleKeyup() :
+        handleClick();
+};
 window.addEventListener('keyup', (e) => insertValueClick(e));
+getAllButtons.forEach(button => {
 
-const insertValueClick = (e) => {
-    if (e.type == 'keyup') {
-        const key = e.key;
-        if (!isNaN(+key) || key == '/' || key == '*' || key == '-' || key == '+' || key == '.')
-            calc.valueVerification(key);
+    const functionButton = button.classList[0];
 
-        else if (key == 'Backspace')
-            calc.removeLastValue();
-
-        else if (key == 'Enter')
-            calc.calculateEquation();
-    }
-    else {
-        const key = e.target.classList[2];
-        calc.valueVerification(key);
-    }
-}
-
-const getAllKeys = Array.from(document.querySelectorAll('button'));
-getAllKeys.forEach(keyButton => {
-    switch (keyButton.classList[0]) {
+    switch (functionButton) {
         case 'value':
-            keyButton.addEventListener('click', (e) => insertValueClick(e));
+            button.addEventListener('click', (e) => insertValueClick(e));
             break;
 
         case 'delete':
-            keyButton.addEventListener('click', () => calc.removeLastValue());
+            button.addEventListener('click', () => calc.removeLastValue());
             break;
 
         case 'reset':
-            keyButton.addEventListener('click', () => calc.resetEquation());
+            button.addEventListener('click', () => calc.resetEquation());
             break;
 
         case 'equal':
-            keyButton.addEventListener('click', () => calc.calculateEquation());
+            button.addEventListener('click', () => calc.calculateEquation());
             break;
 
         default:
